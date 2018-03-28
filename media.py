5258aeb3d0a574cbb3cc53ce65d0c64f995f5897
nogbazg <nogbazghi@gmail.com>
2018-03-28 10:39:49 -0400

    items

--- media.py
+++ media.py
@@ -3,11 +3,11 @@ import webbrowser
 #this is the movie object, contains the constructor, with the different attributes needed to describe the movie
 #These attributes will then be used by fresh_tomatoes
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__(self, title, storyline, poster_image, trailer_youtube):
+        self.movie_title = title
+        self.movie_storyline = storyline
+        self.movie_poster_image_url = poster_image
+        self.movie_trailer_youtube_url = trailer_youtube
     #displays the trailer
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        webbrowser.open(self.movie_trailer_youtube_url)
